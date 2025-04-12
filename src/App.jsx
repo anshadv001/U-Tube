d388292b29084b616f8bd55adb2f1db4b7867001
anshad.v <anshadv001@gmail.com>
2025-04-12 16:34:02 +0530

    category pills developed

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,17 @@
+import CategoryPills from "./Components/CategoryPills";
 import PageHeader from "./layouts/PageHeader";
 
 function App() {
   return (
     <div className="max-h-screen w-screen flex flex-col">
-      <PageHeader/>
-      <div className="grid grid-col-[auto,1fr] flex-grow-1 overflow-auto">
-         <div>sidebar</div>
-         <div className="">
-
-         </div>
+      <PageHeader />
+      <div className="grid grid-cols-[auto_1fr] grow overflow-auto">
+        <div className="">sidebar</div>
+        <div className="overflow-x-hidden px-8 pb-4 ">
+          <div className="sticky top-0 bg-white z-10 pb-4">
+            <CategoryPills/>
+          </div>
+        </div>
       </div>
     </div>
   );
---
+++ src/Components/CategoryPills.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+import Button from "./Button";
+import { categories } from "../assets/data/home";
+
+const CategoryPills = () => {
+  return (
+    <div className="overflow-x-auto relative [scrollbar-width:none] [&::-webkit-scrollbar]:hidden">
+      <div className="whitespace-nowrap gap-3 flex transition-transform w-[max-content]">
+        {categories.map((category) => {
+          return (
+            <Button
+            key={category}
+              variant="default"
+              className="py-1 text-sm px-3 rounded-lg whitespace-nowrap"
+            >
+              {category}
+            </Button>
+          );
+        })}
+      </div>
+    </div>
+  );
+};
+
+export default CategoryPills;
